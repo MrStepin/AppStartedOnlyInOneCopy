a2ea2ebe5c4b8da5d727f9b47f89f8de53ccf125
Стёпин Станислав Александрович <stanislavalex@mail.ru>
2021-11-10 23:03:13 +0300

    .

--- AppStartedOnlyInOneCopy/Program.cs
+++ AppStartedOnlyInOneCopy/Program.cs
@@ -10,22 +10,21 @@ namespace AppStartedOnlyInOneCopy
     class Program
     {
         static bool newAppIsOpened;
+        static string myApp = "MyApp";
+
         static void Main(string[] args)
-        {
-            string myApp = "MyApp";
-            
+        {          
             Mutex mutex = new Mutex(true, myApp, out newAppIsOpened);
+            if (!newAppIsOpened)
+            {
+                return;
+            }
             for (int i = 0; i < 1000; i++)
             {
                 Console.WriteLine("App started!");
                 Thread.Sleep(500);
-                //if (newAppIsOpened)
-                //{
-                //    Environment.Exit(0);
-                //}
             }
             Console.ReadKey();
-            
         }
     }
 }
